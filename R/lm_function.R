d8ce2cfa77f5462ac977821d430ae837254d60f9
AdaemmerP <adaemmer@hsu-hh.de>
2018-04-12 14:13:28 +0200

    New lm_function

---
+++ R/lm_function.R
@@ -0,0 +1,22 @@
+#' Creates a data frame with lagged exogenous variables.
+#'
+#' @param data A data frame.
+#' @param lags The number of lags.
+#' @return A data frame
+
+
+lm_function  <-  function(m){
+
+  if(specs$lags_criterion == ''){
+    data_lm       <- cbind(y = m, x_lin)
+
+                } else {
+
+    data_lm   <- cbind(y = m, x_lin[[specs$lags_lin]])}
+
+  names(data_lm)[1] <- 'y'
+
+  lm_output         <- lm(y ~ ., data = data_lm )
+
+
+}
