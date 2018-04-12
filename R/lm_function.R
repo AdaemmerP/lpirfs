6dfd7617249ab1f3de55181d7470dc2a4d9bc4a1
AdaemmerP <adaemmer@hsu-hh.de>
2018-04-12 15:08:26 +0200

    Description of lm_function

--- R/lm_function.R
+++ R/lm_function.R
@@ -1,19 +1,20 @@
-#' Creates a data frame with lagged exogenous variables.
+#' Function to estimate VAR by OLS: equation for equation.
 #'
-#' @param data A data frame.
-#' @param lags numeric: The number of lags.
-#' @return A data frame.
+#' @param y_data A data frame for the exogenous variables.
+#' @param x_lin A data frame with exogenous variables.
+#' @param lags Numeric value with number of lags.
+#' @return List with output from lm object.
 
 
 
-lm_function  <-  function(m){
+lm_function  <-  function(y_data, x_lin, specs){
 
   if(specs$lags_criterion == ''){
-    data_lm       <- cbind(y = m, x_lin)
+    data_lm       <- cbind(y = y_data, x_lin)
 
                 } else {
 
-    data_lm   <- cbind(y = m, x_lin[[specs$lags_lin]])}
+    data_lm   <- cbind(y = y_data, x_lin[[specs$lags_lin]])}
 
   names(data_lm)[1] <- 'y'
 
