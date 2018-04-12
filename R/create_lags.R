f83aa39e3a0f5044c937834f20f45bf4b17c864e
AdaemmerP <adaemmer@hsu-hh.de>
2018-04-12 14:02:42 +0200

    First commit

---
+++ R/create_lags.R
@@ -0,0 +1,25 @@
+#' Creates a data frame with lagged exogenous variables.
+#'
+#' @param data A data frame.
+#' @param lags The number of lags.
+#' @return A data frame
+
+
+create_lags <- function(data, lags){
+
+  for (i in 1:lags){
+
+    lags_column  <- data                                  %>%
+                      dplyr::mutate_all(funs(lag(., i))) %>%
+                      dplyr::rename_all(funs(paste0(.,"_", "lag_", i)))
+
+    ifelse(i == 1, lag_data <- lags_column, '')
+    ifelse(i > 1,  lag_data <- cbind(lag_data, lags_column), '')
+
+  }
+
+  # Delete NAs
+  lag_data <- lag_data %>%
+                 na.omit()
+
+}
