a39662885fca2011788b1abb9e6a63e73f965fd5
crunchBernd <p.adaemmer@gmail.com>
2018-04-13 15:31:46 +0200

    Test for next commit

--- R/create_lags.R
+++ R/create_lags.R
@@ -4,21 +4,21 @@
 #' @param lags Numeric value for the number of lags.
 #' @return A data frame.
 
-create_lags <- function(data, lags){
+create_lags  <- function(data, lags){
 
   for (i in 1:lags){
 
-    lags_column  <- data                                  %>%
-                      dplyr::mutate_all(funs(lag(., i))) %>%
-                      dplyr::rename_all(funs(paste0(.,"_", "lag_", i)))
+        lags_column  <- data %>%
+                          dplyr::mutate_all(funs(lag(., i))) %>%
+                          dplyr::rename_all(funs(paste0(.,"_", "lag_", i)))
 
-    ifelse(i == 1, lag_data <- lags_column, '')
-    ifelse(i > 1,  lag_data <- cbind(lag_data, lags_column), '')
+        ifelse(i == 1, lag_data <- lags_column, '')
+
+        ifelse(i > 1,  lag_data <- cbind(lag_data, lags_column), '')
 
   }
 
   # Delete NAs
   lag_data <- lag_data %>%
-                 na.omit()
-
+                na.omit()
 }
