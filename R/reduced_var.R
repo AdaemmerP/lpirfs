25274860bc87423f70265d217fb80fb0b89913bb
AdaemmerP <adaemmer@hsu-hh.de>
2018-04-26 12:02:23 +0200

    Updated matrices

--- R/reduced_var.R
+++ R/reduced_var.R
@@ -2,7 +2,7 @@
 #'
 #' @param y_lin A data frame for the exogenous variables.
 #' @param x_lin A list with exogenous variables.
-#' @param data_set_estim Data with all endogenous variables.
+#' @param data_set_df Data frame with all endogenous variables.
 #' @param specs A list with specifications that go into 'lin_lp' or 'nl_lp' function.
 #'        specs$lags_criterion: Either NaN (given lag length) or 'BIC'|'AIC'
 #'        specs$lags_lin:       Lag length
@@ -13,46 +13,65 @@
 #' @author Philipp Adämmer
 
 
-reduced_var  <- function(y_lin, x_lin, data_set_estim, specs){
+reduced_var  <- function(y_lin, x_lin, data_set_df, specs){
 
-  # Estimate OLS equation for equation
-  if (is.nan(specs$lags_criterion) == TRUE) {
+ # Check whether lag criterion is given
+ if (is.nan(specs$lags_criterion) == TRUE) {
 
-    # Estimates reduced VAR with pre-defined lag length
-    lm_all        <- dplyr::as_tibble(lapply(y_lin, lm_function, x_lin))
+  # Estimates reduced VAR with pre-defined lag length
+    y_data        <- lapply(seq_len(ncol(y_lin)), function(i) y_lin[,i])
+    resids_all    <- (lapply(y_data, lm_function, x_lin))
 
-                               } else {
+################################################################################
+                                } else {
+################################################################################
 
-    # Estimates reduced VAR with AIC or BIC criterion
-    lag_criterion <- vars::VARselect(data_set_estim, lag.max =  specs$max_lags)
+ # Estimate lag criteria with VARselect from vars package
+  lag_criterion <- vars::VARselect(data_set_df, lag.max =  specs$max_lags)
 
-    if (specs$lags_criterion == 'AIC'){
+  if (specs$lags_criterion == 'AICc'){
+
+    # Estimate and save AIC values
+    AIC_values  <- lag_criterion$criteria[1,]
+
+    # Calculate number of 'exogenous' variables and add 2 (constant and variance)
+    p           <- unlist(lapply(1:specs$max_lags, function(lag, K){lag*K},
+                                   ncol(data_set_df))) + 2
+
+    # Calculate number of observations for each regression
+    n           <- unlist(lapply(1:specs$max_lags, function(lag, data){
+                                   length(data[(lag+1):dim(data)[1], 1])},
+                                   as.matrix(data_set_df[,1])))
+
+
+    # Calculate AICc, see Cavanaugh (1997) <doi:10.1016/S0167-7152(96)00128-9>
+    #                     Burnham et. al (2011) <doi:10.1007/s00265-010-1029-6>
+      specs$lags_lin  <- which.min(AIC_values + 2*p*(p+1)/n - p - 1)
+
+
+              } else if (specs$lags_criterion == 'AIC'){
+
 
       specs$lags_lin  <- which.min(lag_criterion$criteria[1,])
 
-                               } else {
+                       } else {
 
       specs$lags_lin  <- which.min(lag_criterion$criteria[3,]) }
 
 
     # Build data based on 'optimal lag length
-    y_data <- as_tibble(y_lin[[specs$lags_lin]])
-    x_data <- as_tibble(x_lin[[specs$lags_lin]])
+    y_data      <- as.list(dplyr::as_tibble(y_lin[[specs$lags_lin]]))
+    x_data      <- x_lin[[specs$lags_lin]]
 
-    # Estimate OLS model
-    lm_all  <- dplyr::as_tibble(lapply(y_data, lm_function, x_data))
+    # Estimate OLS model and calculate residuals
+    resids_all  <- lapply(y_data, lm_function, x_data)
 
   }
 
+  # Make matrix of residuals
+  resid_all     <- matrix(unlist(resids_all), ncol = specs$endog, byrow = F )
 
-  # Rename columns
-  names(lm_all) <- 1:specs$endog
-
-  # Extract residuals
-  resid_all     <- matrix(unlist(lm_all[2,]), ncol = specs$endog)
-
-
-  # Make Covariance matrix
+  # Make covariance matrix
   cov_var       <- cov(resid_all)
 
   # Cholesky decomposition
@@ -62,23 +81,23 @@ reduced_var  <- function(y_lin, x_lin, data_set_estim, specs){
   # Shock Matrix
   d <- matrix(NaN, specs$endog, specs$endog)
 
-  # Loop to create shocks
+      if (specs$shock_type == 1){
 
-  if (specs$shock_type == 1){
+        for (i in 1:specs$endog){
 
-    for (i in 1:specs$endog){
-      d[, i]     <-  A[, i]/A[i, i]*D[i, i]
-    }
+             d[, i]     <-  A[, i]/A[i, i]*D[i, i]
 
-  } else {
+        }
 
-    for (i in 1:specs$endog){
-      d[, i]     <-  A[, i]/A[i, i]
+                } else {
 
-    }
-  }
+        for (i in 1:specs$endog){
+          d[, i]     <-  A[, i]/A[i, i]
+
+        }
+      }
 
   # Return shock matrix
-  d
+  return(d)
 
 }
