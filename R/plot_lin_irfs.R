06d52988c6e3ec6c975d34e86a13cd6b73cda5d6
AdaemmerP <adaemmer@hsu-hh.de>
2018-04-26 17:49:15 +0200

    New plot functions

---
+++ R/plot_lin_irfs.R
@@ -0,0 +1,54 @@
+#' Function to plot irfs, estimated with lin_lp function
+#'
+#' @param results_lin List with results from lp_lin function
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_lin_irfs <- function(results_lin){
+
+  irf_lin_mean <- results_lin[[1]]
+  irf_lin_low  <- results_lin[[2]]
+  irf_lin_up   <- results_lin[[3]]
+
+  plot_num     <- 1
+  gg_lin       <- rep(list(NaN), specs$endog*specs$endog)
+
+  # Loop to fill to create plots
+  for(rr in 1:(specs$endog)){
+    for (ss in 1:(specs$endog)){
+
+  # Tibbles for linear irfS
+      tbl_lin_mean <- as.matrix(t(irf_lin_mean[, 1:specs$hor , ss]))[, rr]
+      tbl_lin_low  <- as.matrix(t(irf_lin_low[,  1:specs$hor , ss]))[, rr]
+      tbl_lin_up   <- as.matrix(t(irf_lin_up[,   1:specs$hor , ss]))[, rr]
+
+      tbl_lin      <- tibble(x   = 0:(specs$hor - 1),  mean = tbl_lin_mean,
+                             low   = tbl_lin_low, up = tbl_lin_up)
+
+
+   # Make ggplots
+      gg_lin[[plot_num]] <- ggplot()+
+        geom_line(data     = tbl_lin, aes(y = mean, x = x)) +
+        geom_ribbon(data   = tbl_lin, aes(x = x, ymin = low, ymax = up), col = 'grey',
+                    fill = 'grey', alpha = 0.6) +
+        theme_classic() +
+        ggtitle(paste(specs$columns[ss], 'on', specs$columns[rr], sep=" ")) +
+        xlab('') +
+        ylab('') +
+        theme(title = element_text(size = 6)) +
+        scale_y_continuous(expand = c(0, 0))  +
+        scale_x_continuous(expand = c(0, 0),
+                           breaks = seq(0, specs$hor - 1, 2))  +
+        geom_hline(yintercept = 0, col = "red")
+
+    # Add one to count variable
+      plot_num     <- plot_num + 1
+
+    } }
+
+  # Plot results
+  lin_plots <- sapply(gg_lin, ggplotGrob)
+  marrangeGrob(lin_plots, nrow = specs$endog, ncol = specs$endog)
+}
---
+++ R/plot_nl_irfs.R
@@ -0,0 +1,91 @@
+#'  Function to plot irfs, estimated with lp_lin function
+#'
+#' @param results_nl List with estimated results from lp_nl function
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_nl <- function(results_nl){
+
+  irf_s1_mean <- results_nl[[1]]
+  irf_s1_low  <- results_nl[[2]]
+  irf_s1_up   <- results_nl[[3]]
+
+  irf_s2_mean <- results_nl[[4]]
+  irf_s2_low  <- results_nl[[5]]
+  irf_s2_up   <- results_nl[[6]]
+
+  gg_s1      <- rep(list(NaN), specs$endog*specs$endog)
+  gg_s2      <- rep(list(NaN), specs$endog*specs$endog)
+  plot_num   <- 1
+
+  for(rr in 1:(specs$endog)){
+    for (ss in 1:(specs$endog)){
+
+      # Tibbles for expansion irfs
+      tbl_s1_mean <- as.matrix(t(irf_s1_mean[,  1:specs$hor , ss]))[, rr]
+      tbl_s1_low  <- as.matrix(t(irf_s1_low[,   1:specs$hor , ss]))[, rr]
+      tbl_s1_up   <- as.matrix(t(irf_s1_up[,    1:specs$hor , ss]))[, rr]
+
+      tbl_s1     <- tibble(x   = 1:specs$hor,  mean = tbl_s1_mean,
+                           low = tbl_s1_low, up    = tbl_s1_up)
+
+      # Tibbles for recessions irfs
+      tbl_s1_mean <- as.matrix(t(irf_s2_mean[,  1:specs$hor , ss]))[, rr]
+      tbl_s2_low  <- as.matrix(t(irf_s2_low[,   1:specs$hor , ss]))[, rr]
+      tbl_s2_up   <- as.matrix(t(irf_s2_up[,    1:specs$hor , ss]))[, rr]
+
+      tbl_s2      <- tibble(x   = 1:specs$hor,  mean   = tbl_s1_mean,
+                            low = tbl_s2_low, up      = tbl_s2_up)
+
+
+      gg_s1[[plot_num]] <- ggplot() +
+        geom_line(data     = tbl_s1, aes(y = mean, x = x), col = 'darkgreen') +
+        geom_ribbon(data   = tbl_s1, aes(x = x, ymin = low, ymax = up), col = 'grey',
+                    fill = 'grey', alpha = 0.6) +
+        theme_classic() +
+        ggtitle(paste(specs$columns[ss], 'on', specs$columns[rr], sep=" ")) +
+        xlab('') +
+        ylab('') +
+        theme(title = element_text(size = 6)) +
+        scale_y_continuous(expand = c(0, 0))  +
+        scale_x_continuous(expand = c(0, 0))  +
+        geom_hline(yintercept = 0, col = "red", size = .02)
+
+
+      gg_s2[[plot_num]] <- ggplot() +
+        geom_line(data     = tbl_s2, aes(y = mean, x = x), col = 'darkred') +
+        geom_ribbon(data   = tbl_s2, aes(x = x, ymin = low, ymax = up), col = 'grey',
+                    fill = 'grey', alpha = 0.6) +
+        theme_classic() +
+        ggtitle(paste(specs$columns[ss], 'on', specs$columns[rr], sep=" ")) +
+        xlab('') +
+        ylab('') +
+        theme(title = element_text(size = 6)) +
+        scale_y_continuous(expand = c(0, 0))  +
+        scale_x_continuous(expand = c(0, 0))  +
+        geom_hline(yintercept = 0, col = "red")
+
+
+
+
+
+      plot_num <- plot_num + 1
+
+
+    }
+
+  }
+
+  # Save plots
+  s1_plots <- sapply(gg_s1, ggplotGrob)
+  s2_plots <- sapply(gg_s2, ggplotGrob)
+
+  # Plot plots
+  marrangeGrob(plot_results$s1_plots, nrow = specs$endog, ncol = specs$endog)
+  marrangeGrob(plot_results$s2_plots, nrow = specs$endog, ncol = specs$endog)
+
+  #list (s1_plots = s1_plots, s2_plots = s2_plots)
+
+}
