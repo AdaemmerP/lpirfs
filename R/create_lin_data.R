19157c57374266773cd1e483c66912d706b35697
crunchBernd <p.adaemmer@gmail.com>
2018-04-26 12:02:23 +0200

    Updated matrices

--- R/create_lin_data.R
+++ R/create_lin_data.R
@@ -4,10 +4,10 @@
 #'        specs$lags_criterion: Either NaN (given lag length) or 'AICc'|'AIC'|'BIC'|
 #'        specs$lags_lin:       Lag length
 #'        specs$trend:          1 (no trend), 2 (trend), 3 (quadratic trend)
-#'        specs$max_lags:       Maximum number of lags to use for lag length criteria
+#'        specs$max_lags:       Maximum number of lags to determine lag length criteria
 #' @param data_set_df A data frame with all endogenous variables for VAR.
 #' @export
-#' @return List with left (y_lin) and right hand side (x_lin) variables
+#' @return List with filled matrices of lagged left (y_lin) and right hand side (x_lin) variables
 #' @author Philipp Adämmer
 
 
@@ -28,49 +28,49 @@ create_lin_data     <- function(specs, data_set_df){
            x_lin            <-   x_lin               %>%
                                               as.matrix(),
 
-           x_lin            <-   x_lin               %>%
-             dplyr::mutate(trend    = row_number()   %>%
-                                              as.matrix()),
+           x_lin            <-   x_lin                                     %>%
+                                   dplyr::mutate(trend    = row_number()   %>%
+                                                                   as.matrix()),
 
            x_lin            <-   x_lin               %>%
-             dplyr::mutate(trend    = row_number())  %>%
-             dplyr::mutate(sq_trend = trend^2)       %>%
-                                               as.matrix())
+                                   dplyr::mutate(trend    = row_number())  %>%
+                                   dplyr::mutate(sq_trend = trend^2)       %>%
+                                                                   as.matrix())
 
 ################################################################################
                                } else {
 ################################################################################
 
-    # Create list to store lagged data
-    y_lin     <- rep(list(NaN), specs$max_lags)
-    x_lin     <- rep(list(NaN), specs$max_lags)
+ # Create list to store lagged data
+  y_lin     <- rep(list(NaN), specs$max_lags)
+  x_lin     <- rep(list(NaN), specs$max_lags)
 
-    # Make lag data based on max lag lengths
-    for(i in 1:specs$max_lags){
+ # Make lag data based on max lag lengths
+  for(i in 1:specs$max_lags){
 
-      y_lin[[i]] <-  data_set_df[(i + 1):dim(data_set_df)[1],]   %>%
-        as.matrix()
+    y_lin[[i]] <-  data_set_df[(i + 1):dim(data_set_df)[1],]   %>%
+                                                          as.matrix()
 
-      x_lin[[i]] <-  dplyr::as_tibble(create_lags(data_set_df, i))
+    x_lin[[i]] <-  dplyr::as_tibble(create_lags(data_set_df, i))
 
-      # Include no trend, trend or quadratic trend
-      switch(specs$trend,
+   # Include no trend, trend or quadratic trend
+   switch(specs$trend,
 
-             x_lin[[i]]            <-   x_lin[[i]]      %>%
-               as.matrix(),
-             x_lin[[i]]            <-   x_lin[[i]]                                 %>%
-               dplyr::mutate(trend = row_number())   %>%
-               as.matrix(),
-             x_lin[[i]]            <-   x_lin[[i]]                                 %>%
-               dplyr::mutate(trend = row_number())   %>%
-               dplyr::mutate(sq_trend = trend^2) %>%
-               as.matrix())
+             x_lin[[i]]            <-   x_lin[[i]]                 %>%
+                                                            as.matrix(),
 
+             x_lin[[i]]            <-   x_lin[[i]]                             %>%
+                                          dplyr::mutate(trend = row_number())  %>%
+                                                            as.matrix(),
 
+             x_lin[[i]]            <-   x_lin[[i]]                 %>%
+                                          dplyr::mutate(trend = row_number())   %>%
+                                          dplyr::mutate(sq_trend = trend^2) %>%
+                                                            as.matrix())
     }
   }
 
   # Return list with exogenous and endogenous data
-  list(y_lin, x_lin)
+  return(list(y_lin, x_lin))
 
 }
