4b7b95eaea3674bb25d02deb2c7f4a05e902bc58
AdaemmerP <adaemmer@hsu-hh.de>
2018-04-26 22:12:12 +0200

    Plot functions

--- R/plot_nl_irfs.R
+++ R/plot_nl_irfs.R
@@ -6,7 +6,7 @@
 #' @export
 #'
 #' @examples
-plot_nl <- function(results_nl){
+plot_nl_irfs <- function(results_nl){
 
   irf_s1_mean <- results_nl[[1]]
   irf_s1_low  <- results_nl[[2]]
@@ -29,7 +29,7 @@ plot_nl <- function(results_nl){
       tbl_s1_up   <- as.matrix(t(irf_s1_up[,    1:specs$hor , ss]))[, rr]
 
       tbl_s1     <- tibble(x   = 1:specs$hor,  mean = tbl_s1_mean,
-                           low = tbl_s1_low, up    = tbl_s1_up)
+                           low = tbl_s1_low,   up   = tbl_s1_up)
 
       # Tibbles for recessions irfs
       tbl_s1_mean <- as.matrix(t(irf_s2_mean[,  1:specs$hor , ss]))[, rr]
@@ -37,39 +37,35 @@ plot_nl <- function(results_nl){
       tbl_s2_up   <- as.matrix(t(irf_s2_up[,    1:specs$hor , ss]))[, rr]
 
       tbl_s2      <- tibble(x   = 1:specs$hor,  mean   = tbl_s1_mean,
-                            low = tbl_s2_low, up      = tbl_s2_up)
+                            low = tbl_s2_low,   up     = tbl_s2_up)
 
 
       gg_s1[[plot_num]] <- ggplot() +
-        geom_line(data     = tbl_s1, aes(y = mean, x = x), col = 'darkgreen') +
-        geom_ribbon(data   = tbl_s1, aes(x = x, ymin = low, ymax = up), col = 'grey',
-                    fill = 'grey', alpha = 0.6) +
-        theme_classic() +
-        ggtitle(paste(specs$columns[ss], 'on', specs$columns[rr], sep=" ")) +
-        xlab('') +
-        ylab('') +
-        theme(title = element_text(size = 6)) +
-        scale_y_continuous(expand = c(0, 0))  +
-        scale_x_continuous(expand = c(0, 0))  +
-        geom_hline(yintercept = 0, col = "red", size = .02)
+                  geom_line(data     = tbl_s1, aes(y = mean, x = x), col = 'darkgreen') +
+                  geom_ribbon(data   = tbl_s1, aes(x = x, ymin = low, ymax = up), col = 'grey',
+                              fill = 'grey', alpha = 0.6) +
+                  theme_classic() +
+                  ggtitle(paste(specs$columns[ss], 'on', specs$columns[rr], sep=" ")) +
+                  xlab('') +
+                  ylab('') +
+                  theme(title = element_text(size = 6)) +
+                  scale_y_continuous(expand = c(0, 0))  +
+                  scale_x_continuous(expand = c(0, 0))  +
+                  geom_hline(yintercept = 0, col = "red", size = .02)
 
 
       gg_s2[[plot_num]] <- ggplot() +
-        geom_line(data     = tbl_s2, aes(y = mean, x = x), col = 'darkred') +
-        geom_ribbon(data   = tbl_s2, aes(x = x, ymin = low, ymax = up), col = 'grey',
-                    fill = 'grey', alpha = 0.6) +
-        theme_classic() +
-        ggtitle(paste(specs$columns[ss], 'on', specs$columns[rr], sep=" ")) +
-        xlab('') +
-        ylab('') +
-        theme(title = element_text(size = 6)) +
-        scale_y_continuous(expand = c(0, 0))  +
-        scale_x_continuous(expand = c(0, 0))  +
-        geom_hline(yintercept = 0, col = "red")
-
-
-
-
+                  geom_line(data     = tbl_s2, aes(y = mean, x = x), col = 'darkred') +
+                  geom_ribbon(data   = tbl_s2, aes(x = x, ymin = low, ymax = up), col = 'grey',
+                              fill = 'grey', alpha = 0.6) +
+                  theme_classic() +
+                  ggtitle(paste(specs$columns[ss], 'on', specs$columns[rr], sep=" ")) +
+                  xlab('') +
+                  ylab('') +
+                  theme(title = element_text(size = 6)) +
+                  scale_y_continuous(expand = c(0, 0))  +
+                  scale_x_continuous(expand = c(0, 0))  +
+                  geom_hline(yintercept = 0, col = "red")
 
       plot_num <- plot_num + 1
 
@@ -83,8 +79,8 @@ plot_nl <- function(results_nl){
   s2_plots <- sapply(gg_s2, ggplotGrob)
 
   # Plot plots
-  marrangeGrob(plot_results$s1_plots, nrow = specs$endog, ncol = specs$endog)
-  marrangeGrob(plot_results$s2_plots, nrow = specs$endog, ncol = specs$endog)
+  marrangeGrob(s1_plots, nrow = specs$endog, ncol = specs$endog)
+  marrangeGrob(s2_plots, nrow = specs$endog, ncol = specs$endog)
 
   #list (s1_plots = s1_plots, s2_plots = s2_plots)
 
