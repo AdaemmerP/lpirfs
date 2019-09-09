27bb0b0fcb0d736f3adf3105dbf5d9ce404969a3
AdaemmerP <adaemmer@hsu-hh.de>
2019-09-09 19:59:22 +0200

    New global variables

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 .onLoad <- function(libname, pkgname){
 
  utils::globalVariables(c(".", "trend", "s", "x", "low", "up", "endog_data",
-                          "x_lin", "d", "y_lin", "cross_id", "date_id", ":="))
+                          "x_lin", "d", "y_lin", "cross_id", "date_id", ":=",
+                          "specs", "irf_lin_mean", "irf_lin_low", "irf_lin_up"))
 
 }
