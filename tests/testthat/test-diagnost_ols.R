f2545361afc76c4e3ab3d681cac992c38841bbf8
AdaemmerP <adaemmer@hsu-hh.de>
2022-01-18 21:46:44 +0100

    Indent test

--- tests/testthat/test-diagnost_ols.R
+++ tests/testthat/test-diagnost_ols.R
@@ -6,19 +6,19 @@ test_that("Test whether diagnostic function returns same values as base function
   yy <- matrix(rnorm(100))
 
 # Get diagnostocs for summary()
-get_diagnost              <- lpirfs::ols_diagnost(yy, xx)
-R_sqrd_lpirfs             <- get_diagnost[[3]]
-R_sqrd_adj_lpirfs         <- get_diagnost[[4]]
-F_stat_lpirfs             <- get_diagnost[[5]]
-p_value_F_lpirfs          <- stats::pf(F_stat_lpirfs, get_diagnost[[6]], get_diagnost[[7]], lower.tail = F)
+  get_diagnost              <- lpirfs::ols_diagnost(yy, xx)
+  R_sqrd_lpirfs             <- get_diagnost[[3]]
+  R_sqrd_adj_lpirfs         <- get_diagnost[[4]]
+  F_stat_lpirfs             <- get_diagnost[[5]]
+  p_value_F_lpirfs          <- stats::pf(F_stat_lpirfs, get_diagnost[[6]], get_diagnost[[7]], lower.tail = F)
 
-lm_modeĺ <- lm(yy ~ xx)
-summary_lm <- summary(lm_modeĺ)
-summary_lm$fstatistic
+  lm_modeĺ   <- lm(yy ~ xx)
+  summary_lm <- summary(lm_modeĺ)
+  summary_lm$fstatistic
 
-testthat::expect_equal(R_sqrd_lpirfs, summary_lm$r.squared)
-testthat::expect_equal(R_sqrd_adj_lpirfs, summary_lm$adj.r.squared)
-testthat::expect_equal(F_stat_lpirfs, summary_lm$fstatistic[1], ignore_attr  = T)
+  testthat::expect_equal(R_sqrd_lpirfs, summary_lm$r.squared)
+  testthat::expect_equal(R_sqrd_adj_lpirfs, summary_lm$adj.r.squared)
+  testthat::expect_equal(F_stat_lpirfs, summary_lm$fstatistic[1], ignore_attr  = T)
 
 
 })
